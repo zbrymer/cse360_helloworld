9db6e14c5b9f38b5fa5d7ebb6cbff126032c9971
zbrymer <zbrymer@DESKTOP-U2V3ERU.3521.dhcp.asu.edu>
2024-09-24 16:12:16 -0700

    Changed the Button text and title.

--- ASUHelloWorldJavaFXMavenHW3/ASUHelloWorldJavaFXMavenHW3/src/main/java/edu/asu/ASUHelloWorldJavaFXMaven/HelloFX.java
+++ ASUHelloWorldJavaFXMavenHW3/ASUHelloWorldJavaFXMavenHW3/src/main/java/edu/asu/ASUHelloWorldJavaFXMaven/HelloFX.java
@@ -14,9 +14,9 @@ public class HelloFX extends Application {
     public void start(Stage primaryStage) {
     	System.out.println("ASU Hello World!");
     	System.out.println("It started!");
-        primaryStage.setTitle("Zachary Brymer Hello World Fall 2024");
+        primaryStage.setTitle("In class GitHub activity");
         Button btn = new Button();
-        btn.setText("Display: 'Zachary Brymer says: Hello World!'");
+        btn.setText("Zachary Brymer's second commit");
         btn.setOnAction(new EventHandler<>() {
             public void handle(ActionEvent event) {
                 System.out.println("Zachary Brymer says: Hello World!");
